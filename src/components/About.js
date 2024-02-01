be415dc8142ea79330a793035267f14b1123d4fe
nikhil <nikhilanande58@gmail.com>
2024-02-01 20:44:28 +0530

    my portfolio code updated

---
+++ src/components/About.js
@@ -0,0 +1,40 @@
+// src/components/About.js
+
+import React from "react";
+import "../custom.css";
+
+export default function About() {
+    return (
+      <section id="about" className="homepage-bgimage">
+        <div className="container my-container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
+              We offer comprehensive web solutions.
+            </h1>
+            <p className="about-p mb-8 leading-relaxed text-white">
+            Nik's Techno Web Solutions is the correct choice for all your web solutions. In order to live up to our motto of "Design, Develop, Digital". We devote countless hours to improving websites and developing effective digital marketing tactics.
+            </p>
+            <div className="mydv flex justify-center">
+              <a
+                href="#contact"
+                className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded text-lg">
+                Work With Me
+              </a>
+              <a
+                href="#projects"
+                className="ml-4 inline-flex text-gray-250 bg-gray-600 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg">
+                See My Past Work
+              </a>
+            </div>
+          </div>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+            {/* <img
+              className="object-cover object-center rounded"
+              alt="hero"
+              src="./coding.svg"
+            /> */}
+          </div>
+        </div>
+      </section>
+    );
+  }
---
+++ src/data.js
@@ -0,0 +1,80 @@
+export const projects = [
+  {
+    title: "Laravel React Purity",
+    subtitle: "Laravel and React",
+    description:
+      "Open-source full-stack seed project that uses a React UI powered by a simple Laravel API Server.",
+    image: "./laravel-react.gif",
+    link: "#projects",
+  },
+  {
+    title: "Job Portal",
+    subtitle: "Laravel",
+    description:
+      "A job portal is a website where people who need jobs can find jobs and companies looking for job seekers can find the perfect employees.",
+    image: "./laravel-app.gif",
+    link: "#projects",
+  },
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "React is a declarative, efficient, and flexible JavaScript library for building user interfaces.",
+    image: "./project-1.gif",
+    link: "#projects",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "One library for declarative tracking in React is react-tracking, which implements tracking for each component of the page.",
+    image: "./project-2.gif",
+    link: "#projects",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Firebase is a Backend DataBase service by Team Google that helps a Developer to use it as a Service for the Backend Solution to save and Deploy a Database in Production.",
+    image: "./project-3.gif",
+    link: "#projects",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "React Hooks allows Function components to “hook” into the React state and lifecycle.",
+    image: "./project-4.gif",
+    link: "#projects",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "They are so friendly and no compromise on the quality of the product. They think out of the box and give really valuable suggestion which suites for the real-time environment which makes them trustable. Their support is always appreciable and they are ready to pick up last-minute changes as well.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "They helped us create a CRM module for our company. Great ability to understand client requirements and execute it. Very helpful with suggestions too.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+  "PHP",
+  "Laravel",
+  "MySQL",
+  "Python",
+];
